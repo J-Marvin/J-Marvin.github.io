e98616ce7e4268a3c0abae29f7dc96a02032b4fc
Marvin Lim <jan_marvin_lim@dlsu.edu.ph>
2023-05-16 22:07:06 +0800

    Escaped apostrophe

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -4,7 +4,7 @@ export default function Introduction() {
             <h2 className="text-5xl py-2  text-teal-600 font-medium">Jan Marvin Lim</h2>
             <h3 className="text-2xl py-2">A Developer.</h3>
             <p className="text-md py-5 md:px-10 lg:w-4/6 leading-8 text-gray-800">
-                I'm a Computer Science student currently studying at De La Salle University, Philippines. I aspire to be a software engineer, I love what I do, and I'm excited for the challenges that await me!
+                I&apos;m a Computer Science student currently studying at De La Salle University, Philippines. I aspire to be a software engineer, I love what I do, and I&apos;m excited for the challenges that await me!
             </p>
         </div>
     );
